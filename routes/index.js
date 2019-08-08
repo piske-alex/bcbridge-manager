7df54dbc0038d122b5671b93c268d167fc6fbd4f
Your Name <you@example.com>
2019-08-08 16:00:13 +0800

    change to infura

--- routes/index.js
+++ routes/index.js
@@ -15,10 +15,13 @@ router.get('/wallets', async function (req, res, next) {
   let objarr = await JSON.parse(fs.readFileSync('./hi/store.addresses').toString());
 //console.log(objarr)
   addressesarr = [];
+  let x=0
   for (var key in objarr) {
 
     objarr[key].amount = await bcutils.USDTBalance(objarr[key])
     addressesarr.push(objarr[key]);
+    x++
+    if(x>100)break;
   }
 
   console.log("addresses now:" + addressesarr.length)
